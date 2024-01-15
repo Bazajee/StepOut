520f6f6c2f47736d6ee021f1f0353f490676b6f0
Louis <brousselouis@hotmail.com>
2024-01-15 13:51:52 +0100

    backendinit modif 1

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+const path = require('path');
+
+const app = express()
+
+const facts =  
+app.get('/fact', function(request, response) {
+        response.render('profile', {
+        
+        })
+     })
+
+     app.get('/monument', function(request, response) {
+        response.render('profile', {
+        
+        })
+     })
+
+     app.get('/poi', function(request, response) {
+        response.render('profile', {
+        
+        })
+     })
