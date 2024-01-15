8d4964f259fc1671f28296c67e04a2fdcda209e8
Louis <brousselouis@hotmail.com>
2024-01-15 15:07:16 +0100

    Init des Middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,47 @@
-import express from 'express'
-const path = require('path');
+import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express()
 
-const facts =  
-app.get('/fact', function(request, response) {
-        response.render('profile', {
-        
-        })
-     })
 
-     app.get('/monument', function(request, response) {
-        response.render('profile', {
-        
-        })
-     })
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-     app.get('/poi', function(request, response) {
-        response.render('profile', {
-        
-        })
-     })
+const app = express();
+
+app.get('/monument', (req, res) => {
+    const filePath = join(__dirname, 'data/monument.json');
+    console.log(filePath);
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+app.get('/misc_fact', (req, res) => {
+	const filePath = join(__dirname, 'data/misc_fact.json');
+	console.log(filePath);
+	res.sendFile(filePath, (err) => {
+		 if (err) {
+			  console.error(err);
+			  res.status(500).send('Internal Server Error');
+		 }
+	});
+});
+
+app.get('/poi', (req, res) => {
+	const filePath = join(__dirname, 'data/poi.json');
+	console.log(filePath);
+	res.sendFile(filePath, (err) => {
+		 if (err) {
+			  console.error(err);
+			  res.status(500).send('Internal Server Error');
+		 }
+	});
+});
+
+app.listen(3000, () => {
+    console.log('listening to port 3000');
+});
