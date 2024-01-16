85661d1bf6acd18ed57adfe942f4f62f20d84daf
Louis <brousselouis@hotmail.com>
2024-01-16 14:20:39 +0100

    Ajout de la liste des Pois pour appUse

--- frontend/src/use/useData.js
+++ frontend/src/use/useData.js
@@ -31,3 +31,35 @@ export const monuments = computed(() => {
     // Return null while waiting for the data to be fetched
     return null
 })
+
+
+const  id2poi = ref({})
+const poisListReady = ref(false)
+
+// Computed property to get all pois
+export const pois = computed(() => {
+    // If the poi list data is ready, return all pois as an array
+    if (poisListReady.value) {
+        return Object.values(id2poi.value)
+    }
+    
+    // If the poi list data is not ready, fetch it from the API
+    fetch('/api/poi', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        })
+        .then(response => response.json())
+        .then(poilist => {
+            console.log(poilist)
+            // Populate the id2poi reference with fetched poi data
+            for (const poi of Object.values(poilist)) {
+                id2poi.value[poi.poi_id] = poi
+            }
+            // Set poiList to true to indicate that the data is ready
+            poisListReady.value = true
+        })
+    // Return null while waiting for the data to be fetched
+    return null
+})
