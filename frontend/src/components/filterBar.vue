2d2cdb1e55244a82468857087ff28eb962d2d738
thomassales <salesthomas96@gmail.com>
2024-01-16 15:00:53 +0100

    update filter component in one filterBar.vue

---
+++ frontend/src/components/filterBar.vue
@@ -0,0 +1,52 @@
+<template >
+   <div class="h-full w-full flex flex-row-reverse items-center">
+
+      <div class=" flex w-1/6 " id="filterButton">
+         <button @click="click" class="h-full w-full flex justify-center rounded-lg  hover:border-solid hover:border-2 hover:border-transparent z-40" >
+            <img src="/src/assets/list-thin-svgrepo-com.svg" alt="">
+         </button>
+      </div>
+      <div v-if="isMenuVisible" class=" flex h-3/4 z-40 w-5/6 bg-white justify-center items-center rounded-l">
+         <ul class= "flex  text-black rounded-lg gap-x-4">
+               <li v-for="filter in filters" :key="filter.id" class="flex rounded-full has-[:checked]:bg-indigo-50 has-[:checked]:text-indigo-900 has-[:checked]:ring-indigo-200 ">
+                  <label class="flex gap-2" :for="filter.id"  >
+                     <span> {{ filter.text }}</span>
+                     <input :id="filter.id" type="checkbox" v-model="filter.done">
+                  </label> 
+               </li>
+         </ul>
+      </div>
+
+   </div>
+</template>
+
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const isMenuVisible = ref(false)
+
+
+
+
+
+// replace with use data
+const filters = ref([
+  { id: 1, text: 'Monument', done: false },
+  { id: 2, text: 'Misc fact', done: false },
+])
+
+function click () {
+   isMenuVisible.value = !isMenuVisible.value
+   console.log(isMenuVisible.value)
+}
+
+function filterBarDisplay () {
+   let filterBar = document.getElementById("filterbar")
+   let state = filterBar.classList.includes("hidden") 
+   return console.log(filterBar.classList)
+}
+
+onMounted(() => filterBarDisplay )
+
+</script>
--- frontend/src/views/map.vue
+++ frontend/src/views/map.vue
@@ -1,17 +1,18 @@
 <template>
    <div class="relative h-full w-full"> 
-      <div class="absolute top-0" style="height:100vh; width:100vw; z-index: 2" >
-         <l-map :use-global-leaflet="false" v-model:zoom="zoom" :center="[43.604479, 1.445260]">
+      <div class=" absolute top-0 z-0" style="height:100vh; width:100vw;" >
+         <l-map :options="{zoomControl: false}" :use-global-leaflet="false" v-model:zoom="zoom" :center="[43.604479, 1.445260]">
+            
             <l-tile-layer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png" layer-type="base"
                name="OpenStreetMap"></l-tile-layer>
          </l-map>
       </div>
-      <div class="flex flex-row-reverse" style="z-index: 3;height:10vh; width:100vw;">
-         <div class=" h-full w-1/6 " >
-            <TopLeftButton class="" ></TopLeftButton>
-         </div>
+      <div class="" style="height:10vh; width:100vw;">
+         
+            <filterBar class="" ></filterBar>
+         
 
-         <FilterBar class="w-4/6  bg-white rounded-lg "></FilterBar>
+         <!-- <FilterBar class=" hidden w-4/6  bg-white rounded-lg "></FilterBar> -->
       </div>
    </div>
 </template>
@@ -25,8 +26,8 @@ import router from '/src/router'
 import "leaflet/dist/leaflet.css"
 import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet"
 
-import TopLeftButton from "/src/components/TopLeftButton.vue"
-import FilterBar from "/src/components/FilterBar.vue"
+import filterBar from "/src/components/filterBar.vue"
+// import FilterBar from "/src/components/FilterBar.vue"
 
 const zoom = ref(14)
 
