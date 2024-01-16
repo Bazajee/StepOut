48f2dbea0337344c9920f11bea2e878f39203ec1
Arii D <108074652+ar11tehau@users.noreply.github.com>
2024-01-16 13:16:31 +0100

    current position added

---
+++ frontend/src/views/test.vue
@@ -0,0 +1,59 @@
+<template>
+   <div class="h-screen">
+      <div class="h-fit absolute top-0 z-10 bg-slate-200">
+         <span>Center: {{ center }}</span>
+         <span>Zoom: {{ zoom }}</span>
+         <span>Bounds: {{ bounds }}</span>
+      </div>
+      <l-map class="z-0" :options="options" :zoom="zoom" :center="center" @ready="load" @update:zoom="zoomUpdated" @update:center="centerUpdated" @update:bounds="boundsUpdated">
+         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+         <l-circle-marker :lat-lng="circle.center" :radius="circle.radius" :color="circle.color"/>
+      </l-map>
+   </div>
+</template>
+
+<script setup>
+   import { LCircleMarker, LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
+   import { ref } from 'vue'
+
+   const url = ref('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png')
+   const attribution = ref('&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors')
+   const zoom = ref(16)
+   const bounds = ref(null)
+
+   const center = ref([0, 0])
+   const circle = ref({
+        center: [0, 0],
+        radius: 10,
+        color: 'blue'
+      },)
+
+   const options = ref({zoomControl: false})
+   
+   const successCallback = (position) => {
+      const { latitude, longitude } = position.coords;
+      center.value = [latitude, longitude];
+      circle.value.center = [latitude, longitude];
+      console.log(latitude, longitude);
+   };
+
+   const errorCallback = (error) => {
+      console.log(error);
+   };
+
+   function load() { navigator.geolocation.watchPosition(successCallback, errorCallback); }
+
+
+   function zoomUpdated(NewZoom) {
+      zoom.value = NewZoom;
+   }
+
+   function centerUpdated (NewCenter) {
+      center.value = NewCenter;
+   }
+
+   function boundsUpdated (NewBounds) {
+      bounds.value = NewBounds;
+   }
+
+</script>
