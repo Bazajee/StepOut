a6476d7686173d557b7dbe66e6cfde9ad2abc4f9
Jerome Puch <108734960+Stealer110@users.noreply.github.com>
2024-01-17 15:06:26 +0100

    add missFact + update backen

--- frontend/src/components/poiList.vue
+++ frontend/src/components/poiList.vue
@@ -8,27 +8,34 @@
         <img v-if="url_image_current" class="h-32 w-32" :src="url_image_current" alt="">
         <span class="text-white">{{ selectedMarker }}</span>
     </div>
-
-
  </template>
- <style>
- /* Styles pour le bandeau en bas, positionné au bas de la carte */
 
- </style>
  <script setup>
- import {  LMarker } from '@vue-leaflet/vue-leaflet';
- import { onMounted, ref } from 'vue'
-import {pois, monuments, images, imagesMonuments} from '../use/useData.js'
+    import {  LMarker } from '@vue-leaflet/vue-leaflet';
+    import { onMounted, ref } from 'vue'
+    import {pois, missFacts, imagesMiss_Facts, monuments, images, imagesMonuments} from '../use/useData.js'
 
     const selectedMarker = ref(null)
     const url_image_current = ref('')
     const initMonuments = monuments.value
     const initImage = images.value
     const initMonumentImage = imagesMonuments.value
-    
+    const initimagesMiss_Facts = imagesMiss_Facts.value
+    const initmissFacts = missFacts.value
  
     const getMonumentImageById = (monumentId) => {
         const image = imagesMonuments.value.find(item => item.monument_id == monumentId);
+    if (!image)
+    {
+        return false
+    }
+    const url_image = images.value.find(item => item.id == image.image_id);
+    // Utilisez l'URL complète pour récupérer l'image du backend
+    return url_image ? `api/getImage/${url_image.url}` : false;
+    };
+
+    const getMissFactImageById = (monumentId) => {
+        const image = imagesMiss_Facts.value.find(item => item.misc_fact_id == monumentId);
     if (!image)
     {
         return false
@@ -45,7 +52,8 @@ import {pois, monuments, images, imagesMonuments} from '../use/useData.js'
     };
  
     const getMiscName = (monumentId) => {
-        const monument = Object.values(misc_factList).find(item => item.id == monumentId);
+        const monument = missFacts.value.find(item => item.id == monumentId);
+        url_image_current.value = getMissFactImageById(monument.id)
         return monument ? monument.name : null;
         };
     // Fonction pour définir le marqueur sélectionné
