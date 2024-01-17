d6ac58c6c06e80c2c40145f09d550dfd7eea239d
Arii D <108074652+ar11tehau@users.noreply.github.com>
2024-01-17 14:18:46 +0100

    delete test

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Map from "/src/views/map.vue"
-import Test from "/src/views/test.vue"
 
 const routes = [
   {
     path: '/',
     component: Map,
-  },
-  {
-    path: '/test',
-    component: Test,
   },
   {
     path: "/:catchAll(.*)",
